2ee08138f5d2dcb6e41b6903697988cb445f76c0
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-03-10 14:44:08 -0700

    Update create-AADUserMFAReport.ps1

--- MFA/create-AADUserMFAReport.ps1
+++ MFA/create-AADUserMFAReport.ps1
@@ -1,1 +1,48 @@
+#Requires -module Microsoft.Graph.Authentication
+<#
+.GUID 18c37c40-e24d-4534-8c78-607d6969cb6e
+.AUTHOR Chad.Cox@microsoft.com
+    https://blogs.technet.microsoft.com/chadcox/ (retired)
+    https://github.com/chadmcox
+.Disclaimer
+Use at your own risk
 
+.Description
+This script will go out and pull results about mfa status
+
+#>
+param($resultslocation = "$env:USERPROFILE\Downloads")
+
+cd $resultslocation
+
+Connect-MgGraph -Scopes "Policy.Read.All","Reports.Read.All","AuditLog.Read.All","Directory.Read.All","Directory.Read.All","User.Read.All","AuditLog.Read.All"
+cd $resultslocation
+function getAADUserMFAStatus{
+    [cmdletbinding()] 
+    param()
+    $uri = "https://graph.microsoft.com/beta/reports/credentialUserRegistrationDetails"
+    do{$results = $null
+        for($i=0; $i -le 3; $i++){
+            try{
+                $results = Invoke-MgGraphRequest -Uri $uri -Method GET -OutputType PSObject
+                break
+            }catch{#if this fails it is going to try to authenticate again and rerun query
+                if(($_.Exception.response.statuscode -eq "TooManyRequests") -or ($_.Exception.Response.StatusCode.value__ -eq 429)){
+                    #if this hits up against to many request response throwing in the timer to wait the number of seconds recommended in the response.
+                    write-host "Error: $($_.Exception.response.statuscode), trying again $i of 3"
+                    Start-Sleep -Seconds $_.Exception.response.headers.RetryAfter.Delta.seconds
+                }
+            }
+        }
+        $results.value
+        $uri=$null;$uri = $Results.'@odata.nextlink'
+    }until ($uri -eq $null)
+}
+
+
+
+$results = getAADUserMFAStatus
+$results | export-csv .\AAD_User_MFA_Status.csv -NoTypeInformation
+$results | where {$_.isMFARegistered -eq $true} | export-csv .\AAD_User_Registered_AuthMethods.csv -NoTypeInformation
+
+write-host "Results found here $resultslocation"
