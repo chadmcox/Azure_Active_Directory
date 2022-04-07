f4c7c2e044970da823a2fc9869ba9d7138b486d3
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-07 08:26:02 -0700

    Delete create-AADRecommendedConditionalAccessPolicies.ps1

--- Conditional Access Policy/create-AADRecommendedConditionalAccessPolicies.ps1
+++
@@ -1,5 +0,0 @@
-Connect-MgGraph -Scopes "Policy.Read.All", "Policy.ReadWrite.ConditionalAccess", "Directory.ReadWrite.All", "Directory.AccessAsUser.All"
-
-#Create Tor Exit Note Named Locations
-$body = (invoke-webrequest -uri "https://raw.githubusercontent.com/chadmcox/Azure_Active_Directory/master/Conditional%20Access%20Policy/JSON/Tor_Exit_Notes.json").content | convertfrom-json
-New-MgIdentityConditionalAccessNamedLocation -BodyParameter ($body | convertto-json)
