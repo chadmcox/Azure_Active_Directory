bf568b74b3c905e8fa7f28a77d32c9479890fea2
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-11 08:55:38 -0700

    Delete gatherStaleGuest.ps1

--- Guest/gatherStaleGuest.ps1
+++
@@ -1,6 +0,0 @@
-#looks for logon over the last 30 days
-
-Get-AzureADUser -Filter "userType eq 'Guest'" -All $true | where {$_.userstate -ne 'PendingAcceptance'} | `
-    where {!(Get-AzureADAuditSignInLogs -Filter "userid eq '$($_.objectid)'" -top 1 -ErrorAction SilentlyContinue)} | 
-        select objectid, userprincipalname, Mail,CreationType, UserState, UserStateChangedOn | `
-            export-csv .\guest_no30daylogon.csv -NoTypeInformation
