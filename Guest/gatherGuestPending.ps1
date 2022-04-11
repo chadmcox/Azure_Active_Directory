5ac86c9fa118808250cb7458a9394ac6e059245a
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-11 08:55:31 -0700

    Delete gatherGuestPending.ps1

--- Guest/gatherGuestPending.ps1
+++
@@ -1,2 +0,0 @@
-Get-AzureADUser -Filter "UserState eq 'PendingAcceptance'" -All $true | `
-  select objectid, userprincipalname, Mail,CreationType, UserState, UserStateChangedOn | export-csv .\guest_pending.csv -NoTypeInformation
