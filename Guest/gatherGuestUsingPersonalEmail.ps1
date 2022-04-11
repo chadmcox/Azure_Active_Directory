c5f50168288fa1d63cc52e1e0b5645c32fca9987
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-11 08:55:55 -0700

    Delete gatherGuestUsingPersonalEmail.ps1

--- Guest/gatherGuestUsingPersonalEmail.ps1
+++
@@ -1,3 +0,0 @@
-Get-AzureADUser -Filter "userType eq 'Guest'" -all $true -PipelineVariable guest | `
-    where {$_.UserPrincipalName -match "gmail.com|hotmail.com|msn.com|ymail.com|aol.com|msn.com|outlook.com|live.com|googlemail.com|yahoo.com"} | `
-        select objectid, userprincipalname, Mail,CreationType, UserState, UserStateChangedOn   | export-csv .\guest_personal_email.csv -NoTypeInformation
