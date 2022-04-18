8b8b94f7ac1712b571b02eec3d1282317e85b346
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-18 08:31:20 -0700

    Update find-AADMGUserOldPWD.ps1

--- Users/find-AADMGUserOldPWD.ps1
+++ Users/find-AADMGUserOldPWD.ps1
@@ -1,9 +1,11 @@
+param($defaultpath="$env:USERPROFILE\downloads",$pwdnochangedindays = 480)
+cd $defaultpath
 Connect-MgGraph -Scopes "Directory.ReadWrite.All", "Directory.AccessAsUser.All","User.Read.All","AuditLog.Read.All" 
 Select-MgProfile -Name beta
-
-$pwdnochangedindays = 480
 
 Get-MgUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,passwordPolicies,mail,lastPasswordChangeDateTime | `
     select id,displayName,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,mail,lastPasswordChangeDateTime,passwordPolicies, `
         @{N='LastSignInDateTime';E={$_.signInActivity.LastSignInDateTime}}, @{N='LastNonInteractiveSignInDateTime';E={$_.signInActivity.LastNonInteractiveSignInDateTime}} | `
             where {(New-TimeSpan -Start $_.lastPasswordChangeDateTime -end $(get-date)).TotalDays -gt $pwdnochangedindays} | export-csv .\aad_users_oldpasswords.csv -notypeinformation
+            
+write-host "Report can be found here cd $defaultpath"
