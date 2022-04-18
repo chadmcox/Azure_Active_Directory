fd6f0148dce50609e1cd83e6cc9d1a2b1252f7d2
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-18 08:52:31 -0700

    Update and rename find-AADMGUserOldPWD.ps1 to find-AADMGUserStalePWD.ps1

--- Users/find-AADMGUserStalePWD.ps1
+++ Users/find-AADMGUserStalePWD.ps1
@@ -6,6 +6,6 @@ Select-MgProfile -Name beta
 Get-MgUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,passwordPolicies,mail,lastPasswordChangeDateTime | `
     select id,displayName,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,mail,lastPasswordChangeDateTime,passwordPolicies, `
         @{N='LastSignInDateTime';E={$_.signInActivity.LastSignInDateTime}}, @{N='LastNonInteractiveSignInDateTime';E={$_.signInActivity.LastNonInteractiveSignInDateTime}} | `
-            where {(New-TimeSpan -Start $_.lastPasswordChangeDateTime -end $(get-date)).TotalDays -gt $pwdnochangedindays} | export-csv .\aad_users_oldpasswords.csv -notypeinformation
+            where {(New-TimeSpan -Start $_.lastPasswordChangeDateTime -end $(get-date)).TotalDays -gt $pwdnochangedindays} | export-csv .\aad_users_stalepasswords.csv -notypeinformation
             
 write-host "Report can be found here cd $defaultpath"
