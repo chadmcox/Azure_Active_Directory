588e50fd0dcd7435648c88d40a30331130345294
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-18 08:18:38 -0700

    Update create-AADMGUserReport.ps1

--- Users/create-AADMGUserReport.ps1
+++ Users/create-AADMGUserReport.ps1
@@ -1,1 +1,5 @@
-
+Get-MgUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName, `
+  userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,passwordPolicies,mail,lastPasswordChangeDateTime | `
+    select id,displayName,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,mail,lastPasswordChangeDateTime,passwordPolicies, `
+        @{N='LastSignInDateTime';E={$_.signInActivity.LastSignInDateTime}}, @{N='LastNonInteractiveSignInDateTime';E={$_.signInActivity.LastNonInteractiveSignInDateTime}} | `
+          export-csv .\aad_user_report.csv -notypeinformation
