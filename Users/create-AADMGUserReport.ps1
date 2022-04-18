ad42d11751eee694a5c7106ff5c099731b0aab01
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-18 08:19:09 -0700

    Update create-AADMGUserReport.ps1

--- Users/create-AADMGUserReport.ps1
+++ Users/create-AADMGUserReport.ps1
@@ -1,3 +1,6 @@
+Connect-MgGraph -Scopes "Directory.ReadWrite.All", "Directory.AccessAsUser.All","User.Read.All","AuditLog.Read.All" 
+Select-MgProfile -Name beta
+
 Get-MgUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName, `
   userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,passwordPolicies,mail,lastPasswordChangeDateTime | `
     select id,displayName,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,mail,lastPasswordChangeDateTime,passwordPolicies, `
