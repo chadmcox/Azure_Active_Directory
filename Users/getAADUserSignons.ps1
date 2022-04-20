56946c579289f2b7d02ad1f5f853d47a9e43860d
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-04-20 14:40:17 -0700

    Update getAADUserSignons.ps1

--- Users/getAADUserSignons.ps1
+++ Users/getAADUserSignons.ps1
@@ -41,57 +41,14 @@ param($upn)
     $last_signon_date = (Get-AzureADAuditSignInLogs -Filter "userPrincipalName eq '$upn'" -top 1).CreatedDateTime
     if($last_signon_date){get-date $last_signon_date -Format MM/dd/yyyy}
 }
-function getmailboxtype{
-    param($rtd)
-    #create hash table to decipher code
-    $MSExchRecipientTypeDetails = @{
-            1="UserMailbox"
-            2="LinkedMailbox"
-            4="SharedMailbox"
-            8="LegacyMailbox"
-            16="RoomMailbox"
-            32="EquipmentMailbox"
-            64="MailContact"
-            128="MailUser"
-            256="MailUniversalDistributionGroup"
-            512="MailNonUniversalGroup"
-            1024="MailUniversalSecurityGroup"
-            2048="DynamicDistributionGroup"
-            4096="Public Folder"
-            8192="SystemAttendantMailbox"
-            16384="SystemMailbox"
-            32768="MailForestContact"
-            65536="User"
-            131072="Contact"
-            262144="UniversalDistributionGroup"
-            524288="UniversalSecurityGroup"
-            1048576="NonUniversalGroup"
-            2097152="DisabledUser"
-            4194304="MicrosoftExchange"
-            8388608="ArbitrationMailbox"
-            16777216="MailboxPlan"
-            33554432="LinkedUser"
-            268435456="RoomList"
-            536870912="DiscoveryMailbox"
-            1073741824="RoleGroup"
-            2147483648="RemoteMailbox"
-            137438953472="TeamMailbox"
-            8589934592="RemoteRoomMailbox"
-            17179869184="RemoteEquipmentMailbox"
-            34359738368="RemoteSharedMailbox" }
-            $MSExchRecipientTypeDetails[$rtd]
-}
+
 
 write-host "Retrieving users from AzureAD"
-#$users = get-MsolUser -all -PipelineVariable AADUser | where {$aaduser.usertype -eq "Member"}
+$users = get-azureaduser -all $true
 
 write-host "Populating results"
 @(foreach($user in $users){
-    $user | select objectid, UserPrincipalName,BlockCredential,MSExchRecipientTypeDetails, `
-    @{N="MailboxType";E={getmailboxtype -rtd ($user).MSExchRecipientTypeDetails}}, `
-    @{N="StsRefreshTokensValidFrom";E={get-date $($_.StsRefreshTokensValidFrom) -f MM/dd/yyyy}}, `
-    @{N="LastPasswordChangeTimestamp";E={if($_.LastPasswordChangeTimestamp){
-      get-date $($_.LastPasswordChangeTimestamp) -f MM/dd/yyyy}else{$null}}}, `
+    $user | select objectid, UserPrincipalName, `
     @{N='LastSignOn';E={getaadlastazureadlogon -upn $_.userprincipalname}}
 }) | export-csv $report -NoTypeInformation
 
