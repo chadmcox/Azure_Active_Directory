d0f0cb3be5e0fca8261308de3898109cf9d377f8
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-08-11 13:36:53 -0700

    Update export-MGAADPIMROLEMembersstate.ps1

--- PIM/export-MGAADPIMROLEMembersstate.ps1
+++ PIM/export-MGAADPIMROLEMembersstate.ps1
@@ -9,7 +9,7 @@ function retrieveaadrolemembers{
     param()
     Get-MgPrivilegedAccessRoleDefinition -PrivilegedAccessId AADRoles -Filter "resourceId eq '$($context.TenantId)'" | foreach{
         $role = $null; $role = $_
-        write-host "$($role.DisplayName) $($role.id)"
+        write-host "Exporting $($role.DisplayName) $($role.id)"
         Get-MgPrivilegedAccessRoleAssignment -PrivilegedAccessId AADRoles -Filter "resourceId eq '$($context.TenantId)' and roleDefinitionId eq '$($role.id)'" | `
             select @{N="roleId";E={$role.Id}}, @{N="roleName";E={$role.DisplayName}}, SubjectId, AssignmentState, `
                 @{N="Permanant";E={if($_.AssignmentState -eq "Active" -and $_.EndDateTime -eq $null){$true}else{$false}}}
@@ -21,23 +21,31 @@ function retrieveactualobject{
     Get-MgDirectoryObject -DirectoryObjectId $objectid | select -ExpandProperty AdditionalProperties | Convertto-Json | ConvertFrom-Json | select `
         "@odata.type", displayName, @{N="roleId";E={$members.roleId}}, @{N="roleName";E={$members.roleName}}, `
             @{N="SubjectId";E={$objectid}}, @{N="AssignmentState";E={$members.AssignmentState}}, `
-            @{N="IsMfaRegistered";E={(Get-MgReportAuthenticationMethodUserRegistrationDetail -UserRegistrationDetailsId $objecid).IsMfaRegistered}}, `
+            @{N="IsMfaRegistered";E={(Get-MgReportAuthenticationMethodUserRegistrationDetail -UserRegistrationDetailsId $objectid).IsMfaRegistered}}, `
             @{N="Permanant";E={$members.Permanant}}
 }
-
+function expandgroup{
+    [cmdletbinding()] 
+    param($objectid,$members,$group)
+    write-host "Exporting $($cleanmem.DisplayName) $($cleanmem.id)"
+    $groupmems = Get-MgPrivilegedAccessRoleAssignment -PrivilegedAccessId aadGroups -Filter "resourceId eq '$objectid'" | foreach{
+        retrieveactualobject -objectid $_.subjectid -members $members | select *, @{N="nestedgroup";E={$group}}            
+    }
+    if(!($groupmems)){
+        write-host "Exporting $($cleanmem.DisplayName) $($cleanmem.id)" -ForegroundColor Yellow
+        Get-MgGroupMember -GroupId $cleanmem.SubjectId | foreach{
+            retrieveactualobject -objectid $_.id -members $members | select *, @{N="nestedgroup";E={$group}}
+        }
+    }else{
+        $groupmems
+    }
+}
 
 retrieveaadrolemembers -PipelineVariable members | foreach{
     retrieveactualobject -objectid $members.subjectid -members $members -PipelineVariable cleanmem | foreach {
         $cleanmem | select *, nestedgroup
         if($_."@odata.type" -eq "#microsoft.graph.group"){
-            try{Get-MgPrivilegedAccessRoleAssignment -PrivilegedAccessId aadGroups -Filter "resourceId eq '$($cleanmem.SubjectId)'" | foreach{
-                retrieveactualobject -objectid $_.subjectid -members $members | select *, @{N="nestedgroup";E={$cleanmem.displayName}}            
-            }}
-            catch{
-                Get-MgGroupMember -GroupId $cleanmem.SubjectId | foreach{
-                    retrieveactualobject -objectid $_.id -members $members | select *, @{N="nestedgroup";E={$cleanmem.displayName}}
-                }
-            }
+            expandgroup -objectid $cleanmem.SubjectId -member $members -group $cleanmem.displayName
         }
     }
 } | export-csv .\aadrolemembdershipmfastatus.csv -notypeinformation
