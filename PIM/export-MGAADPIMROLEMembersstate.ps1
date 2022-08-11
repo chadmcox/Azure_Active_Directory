154037c5383c92be24a1a7e16ef4ed911d972e1a
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-08-11 13:03:55 -0700

    Create export-MGAADPIMROLEMembersstate.ps1

---
+++ PIM/export-MGAADPIMROLEMembersstate.ps1
@@ -0,0 +1,43 @@
+Connect-MgGraph
+Select-MgProfile -Name "beta"
+Import-Module Microsoft.Graph.Identity.Governance
+
+$context = get-mgcontext
+
+function retrieveaadrolemembers{
+    [cmdletbinding()] 
+    param()
+    Get-MgPrivilegedAccessRoleDefinition -PrivilegedAccessId AADRoles -Filter "resourceId eq '$($context.TenantId)'" | foreach{
+        $role = $null; $role = $_
+        write-host "$($role.DisplayName) $($role.id)"
+        Get-MgPrivilegedAccessRoleAssignment -PrivilegedAccessId AADRoles -Filter "resourceId eq '$($context.TenantId)' and roleDefinitionId eq '$($role.id)'" | `
+            select @{N="roleId";E={$role.Id}}, @{N="roleName";E={$role.DisplayName}}, SubjectId, AssignmentState, `
+                @{N="Permanant";E={if($_.AssignmentState -eq "Active" -and $_.EndDateTime -eq $null){$true}else{$false}}}
+    }
+}
+function retrieveactualobject{
+    [cmdletbinding()] 
+    param($objectid,$members)
+    Get-MgDirectoryObject -DirectoryObjectId $objectid | select -ExpandProperty AdditionalProperties | Convertto-Json | ConvertFrom-Json | select `
+        "@odata.type", displayName, @{N="roleId";E={$members.roleId}}, @{N="roleName";E={$members.roleName}}, `
+            @{N="SubjectId";E={$objectid}}, @{N="AssignmentState";E={$members.AssignmentState}}, `
+            @{N="IsMfaRegistered";E={(Get-MgReportAuthenticationMethodUserRegistrationDetail -UserRegistrationDetailsId 0042cc77-efb5-4d94-87ff-78328e2fd489).IsMfaRegistered}}, `
+            @{N="Permanant";E={$members.Permanant}}
+}
+
+
+retrieveaadrolemembers -PipelineVariable members | foreach{
+    retrieveactualobject -objectid $members.subjectid -members $members -PipelineVariable cleanmem | foreach {
+        $cleanmem | select *, nestedgroup
+        if($_."@odata.type" -eq "#microsoft.graph.group"){
+            try{Get-MgPrivilegedAccessRoleAssignment -PrivilegedAccessId aadGroups -Filter "resourceId eq '$($cleanmem.SubjectId)'" | foreach{
+                retrieveactualobject -objectid $_.subjectid -members $members | select *, @{N="nestedgroup";E={$cleanmem.displayName}}            
+            }}
+            catch{
+                Get-MgGroupMember -GroupId $cleanmem.SubjectId | foreach{
+                    retrieveactualobject -objectid $_.id -members $members | select *, @{N="nestedgroup";E={$cleanmem.displayName}}
+                }
+            }
+        }
+    }
+} | export-csv .\aadrolemembdershipmfastatus.csv -notypeinformation
