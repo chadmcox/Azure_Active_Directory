ed88e82fd4f3b1c6636f7077d090fb4125147a1d
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-09-13 15:31:35 -0700

    Update runbook-removeAADStaleGuest.ps1

--- Maintenance Task/runbook-removeAADStaleGuest.ps1
+++ Maintenance Task/runbook-removeAADStaleGuest.ps1
@@ -103,7 +103,7 @@ $graphApiHeader = @{ Authorization = "Bearer $graphApiToken" }
 #https://docs.microsoft.com/en-us/graph/api/resources/user?view=graph-rest-beta
 $uri = "https://graph.microsoft.com/beta/users?`$filter=userType eq 'Guest' and externalUserState eq 'Accepted'&`$select=id,displayName,signInActivity,userPrincipalName,externalUserState,externalUserStateChangeDateTime,creationType,createdDateTime,onPremisesSyncEnabled"
 return-AADMSGraph -Uri $uri -pv user | where {!($_.onPremisesSyncEnabled -eq $true)} | where {($_.signInActivity.lastSignInDateTime -eq $null) -or ((New-TimeSpan -Start $_.signInActivity.lastSignInDateTime -end $(get-date)).TotalDays -gt $notsignedonindays)} | `
-    where {($user.signInActivity.lastNonInteractiveSignInDateTime -eq $null) -or ((New-TimeSpan -Start $user.signInActivity.lastNonInteractiveSignInDateTime -end $(get-date)).TotalDays -gt $notsignedonindays)}
+    where {($user.signInActivity.lastNonInteractiveSignInDateTime -eq $null) -or ((New-TimeSpan -Start $user.signInActivity.lastNonInteractiveSignInDateTime -end $(get-date)).TotalDays -gt $notsignedonindays)} | `
         select id,displayName,signInActivity,userPrincipalName -first $removalthreshold | foreach{
             Write-Output "Deleting - $($user.userPrincipalName) : noninteractive $($user.signInActivity.lastSignInDateTime)"
             remove-AADGuestUser -guestid $user.id
