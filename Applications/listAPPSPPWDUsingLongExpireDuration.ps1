4c379c4e7c0154f60724aaafe0e3c32d21977966
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2022-10-19 08:19:29 -0700

    Delete listAPPSPPWDUsingLongExpireDuration.ps1

--- Applications/listAPPSPPWDUsingLongExpireDuration.ps1
+++
@@ -1,9 +0,0 @@
-$now=(get-date).AddDays(730))
-
-Get-AzureADServicePrincipal -Filter "serviceprincipaltype eq 'Application'" -All $true -PipelineVariable aadsp | `
-    select -ExpandProperty PasswordCredentials | where {$_.enddate -gt $now} | select `
-        @{N="ObjectID";E={($aadsp.objectid}}, @{N="DisplayName";E={$aadsp.Displayname}},@{N="Expires";E={$_.enddate}}
-        
-Get-AzureADApplication -All $true -PipelineVariable aadsp | `
-    select -ExpandProperty PasswordCredentials | where {$_.enddate -gt $now} | select `
-        @{N="ObjectID";E={($aadsp.objectid}}, @{N="DisplayName";E={$aadsp.Displayname}},@{N="Expires";E={$_.enddate}}
