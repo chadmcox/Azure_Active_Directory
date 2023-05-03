3029ddeb78433e04e572004f9f0d057c0c5ed173
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-05-03 15:09:21 -0700

    Create Find possible impact if privileged role member is required to use compliant device.ps1

---
+++ Log Analytics/Conditional Access Policy/Privileged Role Members/Find possible impact if privileged role member is required to use compliant device.ps1
@@ -0,0 +1,12 @@
+//
+//
+
+let privusers = pack_array("**replace this with the results from the privuser.txt found from the powershell cmdlets**");
+SigninLogs 
+| where TimeGenerated > ago(14d) and UserId  in~ (privusers) and ResultType == 0 
+| extend trustType = tostring(parse_json(DeviceDetail).trustType) 
+| extend isCompliant = tostring(parse_json(DeviceDetail).isCompliant) 
+| extend TrustedLocation = tostring(iff(NetworkLocationDetails contains 'trustedNamedLocation', 'trustedNamedLocation',''))
+| extend os = tostring(parse_json(DeviceDetail).operatingSystem) 
+| where isCompliant <> 'true' and trustType <> "Hybrid Azure AD joined"  
+| distinct AppDisplayName,UserPrincipalName,ConditionalAccessStatus,AuthenticationRequirement, TrustedLocation, trustType,isCompliant,os, Category
