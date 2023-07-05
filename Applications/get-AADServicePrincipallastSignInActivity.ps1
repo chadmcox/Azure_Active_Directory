a4b32b2ca05480a6db32d1ded01a6e61be27cd6f
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-07-05 15:12:27 -0700

    Update get-AADServicePrincipallastSignInActivity.ps1

--- Applications/get-AADServicePrincipallastSignInActivity.ps1
+++ Applications/get-AADServicePrincipallastSignInActivity.ps1
@@ -1,3 +1,28 @@
+<#PSScriptInfo
+.VERSION 2023.7
+.GUID 368f7248-347a-46d9-ba36-3ae42890daed
+.AUTHOR Chad.Cox@microsoft.com
+    https://blogs.technet.microsoft.com/chadcox/
+    https://github.com/chadmcox
+.COMPANYNAME
+.COPYRIGHT This Sample Code is provided for the purpose of illustration only and is not
+intended to be used in a production environment.  THIS SAMPLE CODE AND ANY
+RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  We grant You a
+nonexclusive, royalty-free right to use and modify the Sample Code and to
+reproduce and distribute the object code form of the Sample Code, provided
+that You agree: (i) to not use Our name, logo, or trademarks to market Your
+software product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is embedded;
+and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and
+against any claims or lawsuits, including attorneys` fees, that arise or result
+from the use or distribution of the Sample 
+
+.NOTE
+This is a beta api make sure
+https://learn.microsoft.com/en-us/graph/api/reportroot-list-serviceprincipalsigninactivities?view=graph-rest-beta&tabs=http
+#>
 param($defaultdirectory="$env:USERPROFILE\Downloads")
 cd $defaultdirectory
 
