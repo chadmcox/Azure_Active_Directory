8d83a9938dd877a1c12554ed90c3c4311582349b
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-07-28 08:59:20 -0700

    Update getAADSPSignInActivity.ps1

--- Applications/getAADSPSignInActivity.ps1
+++ Applications/getAADSPSignInActivity.ps1
@@ -1,3 +1,4 @@
+#Requires -Modules Microsoft.Graph.Authentication,Microsoft.Graph.Beta.Reports,Microsoft.Graph.Beta.Users,Microsoft.Graph.Beta.Applications
 <#PSScriptInfo
 .VERSION 2023.7
 .GUID 368f7248-347a-46d9-ba36-3ae42890daed
@@ -22,6 +23,8 @@ from the use or distribution of the Sample
 .NOTE
 This is a beta api make sure
 https://learn.microsoft.com/en-us/graph/api/reportroot-list-serviceprincipalsigninactivities?view=graph-rest-beta&tabs=http
+
+This script will create a report of all enabled service principals and if they have any recent signin data.
 #>
 param($defaultdirectory="$env:USERPROFILE\Downloads")
 cd $defaultdirectory
@@ -29,12 +32,24 @@ cd $defaultdirectory
 connect-mggraph -scope 'Application.Read.All', 'Directory.Read.All', 'AuditLog.Read.All'
 
 #modules Microsoft.Graph.Authentication,Microsoft.Graph.Beta.Reports,Microsoft.Graph.Beta.Users,Microsoft.Graph.Beta.Applications
-$sps_lastsignin = Get-MgBetaReportServicePrincipalSignInActivity -all
+Write-host "Get all the service principals that have datetime populated"
+$sps_lastsignin = Get-MgBetaReportServicePrincipalSignInActivity -all | where {$_.LastSignInActivity.LastSignInDateTime} | select appid, LastSignInActivity, `
+        @{N="SignInActivityType";E={if($_.delegatedClientSignInActivity.lastSignInDateTime){"delegatedClient"
+        }elseif($_.delegatedResourceSignInActivity.lastSignInDateTime){"delegatedResource"
+        }elseif($_.applicationAuthenticationClientSignInActivity.lastSignInDateTime){"applicationAuthenticationClient"
+        }elseif($_.applicationAuthenticationResourceSignInActivity.lastSignInDateTime){"applicationAuthenticationResource"
+        }else{"unknown"}}}
+write-host "Found SignInActivity for $($sps_lastsignin.count) service principals"
+write-host "Build a hash table for quick lookup"
 $sps_lastsignin_hash = $sps_lastsignin | select appid -ExpandProperty LastSignInActivity | select appid, @{N="LastSignInDateTime";E={$_.LastSignInDateTime}} | group appid -AsHashTable -AsString
 
+write-host "Getting all service principals and create the report"
 Get-MgBetaServicePrincipal -Filter "serviceprincipaltype eq 'Application' and AccountEnabled eq true" -all -ExpandProperty owners | `
     where {!($_.PublisherName -like "*Microsoft*") -or $_.PublisherName -eq "Microsoft Accounts" -and !($_.AppOwnerOrganizationId -eq 'f8cdef31-a31e-4b4a-93e4-5f571e91255a')} | select `
     id, displayname, servicePrincipalType, AccountEnabled, PublisherName, appid, appdisplayname,AppRoleAssignmentRequired,SignInAudience, `
     @{N="createdDateTime";E={[datetime]$_.AdditionalProperties.createdDateTime}}, `
     @{N="LastSignInDateTime";E={[datetime]$sps_lastsignin_hash[$_.appid].LastSignInDateTime}}, `
+    @{N="SignInActivityType";E={$sps_lastsignin_hash[$_.appid].SignInActivityType}}, `
     @{N="Owner";E={($_.owners.id | foreach{Get-mgbetauser -userId $_}).UserPrincipalName -join(";")}} | export-csv .\aadsp_activity.csv -notypeinformation
+
+write-host "finished, results can be found here $($defaultdirectory)\aadsp_activity.csv"
