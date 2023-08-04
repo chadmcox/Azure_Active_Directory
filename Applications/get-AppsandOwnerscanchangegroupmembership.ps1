c544e562c5d65be3b10bfd6940cdb65c60ac7755
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-04 12:24:08 -0700

    Create get-AppsandOwnerscanchangegroupmembership.ps1

---
+++ Applications/get-AppsandOwnerscanchangegroupmembership.ps1
@@ -0,0 +1,12 @@
+connect-mggraph
+
+#get the graph id
+$Graph = Get-MgBetaServicePrincipal -filter "appId eq '00000003-0000-0000-c000-000000000000'"
+#get the permission IDs
+$group_permissions = $Graph | select -ExpandProperty approles | select * | where {$_.value -in ("GroupMember.ReadWrite.All","Group.ReadWrite.All")}
+
+
+Get-MgBetaServicePrincipalAppRoleAssignedTo -ServicePrincipalId $graph.id -All | `
+    where {$_.AppRoleId -in ($group_permissions.id)} | select PrincipalDisplayName, PrincipalId -Unique | foreach{
+        Get-MgBetaServicePrincipal -serviceprincipalid $_.PrincipalId -ExpandProperty owners | select appid, displayname,PublisherName, owners
+    }
