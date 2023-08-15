220cb61e4c1219a7adf1497a912a2f85adb41217
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 08:48:43 -0700

    Add files via upload

---
+++ Applications/AzureADModule/assignedSPOPerms.ps1
@@ -0,0 +1,9 @@
+﻿connect-azuread
+$appids = "00000003-0000-0000-c000-000000000000","00000003-0000-0ff1-ce00-000000000000"
+$Graph = $appids | foreach {Get-AzureADServicePrincipal -filter "appId eq '$($_)'"}
+$permissions = "Sites.FullControl.All","Sites.Manage.All","Sites.Read.All","Sites.ReadWrite.All","Files.Read.All","Files.ReadWrite.All","File.Read.All"
+$app_permissions = $Graph | select -ExpandProperty approles | select * | where {$_.value -in ($permissions)}
+
+$Graph | foreach {Get-AzureADServiceAppRoleAssignment -ObjectId $_.objectid -All $true |  where {$_.id -in $app_permissions.id} | `
+    where {!($_.PrincipalDisplayName -like "*Microsoft Assessments*")} | `
+    select PrincipalType, PrincipalId, PrincipalDisplayName} | export-csv .\spo_assigned_permissions.csv -NoTypeInformation
