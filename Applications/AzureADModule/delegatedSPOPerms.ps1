a934422da1a09cfb63f14253ed4e00cda693e2a5
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 08:46:47 -0700

    Add files via upload

---
+++ Applications/AzureADModule/delegatedSPOPerms.ps1
@@ -0,0 +1,12 @@
+﻿Connect-AzureAD
+
+$permissions = "Sites.FullControl.All","Sites.Manage.All","Sites.Read.All","Sites.ReadWrite.All","Files.Read.All","Files.ReadWrite.All","File.Read.All"
+
+$sps = Get-AzureADServicePrincipal -filter "servicePrincipalType eq 'Application'" -all $true | `
+    where {!($_.PublisherName -like "*Microsoft*") -or $_.PublisherName -eq "Microsoft Accounts" -and !($_.AppOwnerTenantId -eq 'f8cdef31-a31e-4b4a-93e4-5f571e91255a')-and $_.appDisplayname -ne "Microsoft Assessments"}
+
+$sps | where{$sp=$null; $sp=$_
+   Get-AzureADServicePrincipalOAuth2PermissionGrant -ObjectId $sp.objectid -PipelineVariable AADOPG | foreach{
+        $AADOPG.scope -split " " | where {$_ -in $permissions}
+   }
+} | select objectid, displayname, PublisherName, AccountEnabled | export-csv .\spo_delegated_permissions.csv -NoTypeInformation
