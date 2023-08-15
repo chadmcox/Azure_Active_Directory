21c3ab690d0f4f6c6010c4a26f63a4c9f1cf190f
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 10:08:15 -0700

    Update get-SPToMuchPerms.ps1

--- Applications/get-SPToMuchPerms.ps1
+++ Applications/get-SPToMuchPerms.ps1
@@ -1,9 +1,14 @@
-$sps = Get-MgServicePrincipal -all
+param($defaultpath = "$env:USERPROFILE\Downloads")
+if(!(Get-MgContext)){
+    connect-mggraph -scopes "Application.Read.All","Directory.Read.All"
+}
+cd $defaultpath
+$sps = Get-MgBetaServicePrincipal -all
 $graph_hash = $sps | where {$_.approles.AllowedMemberTypes -like "Application"} | select @{Name="Application";Expression={$_.displayname}} -ExpandProperty AppRoles | group id -AsHashTable -AsString
 $sps | where {$_.approles.AllowedMemberTypes -like "Application"} -PipelineVariable s | foreach{
-    Get-MgServicePrincipalAppRoleAssignedTo -ServicePrincipalId $s.id -all | 
+    Get-MgBetaServicePrincipalAppRoleAssignedTo -ServicePrincipalId $s.id -all | 
         where {($graph_hash[$_.AppRoleId]).value -like "*write*"} | foreach{$sp=$null;$sp=$_
-        Get-MgServicePrincipalOwner -ServicePrincipalId $_.PrincipalId | select -ExpandProperty AdditionalProperties | `
+        Get-MgBetaServicePrincipalOwner -ServicePrincipalId $_.PrincipalId | select -ExpandProperty AdditionalProperties | `
             convertto-json | convertfrom-json | foreach{$owner=$null;$owner=$_
             $graph_hash[$sp.AppRoleId] | where {$_.value -like "*write*" -and $_.application -eq $s.displayname} | select `
             @{Name="Owner";Expression={$owner.displayname}}, `
