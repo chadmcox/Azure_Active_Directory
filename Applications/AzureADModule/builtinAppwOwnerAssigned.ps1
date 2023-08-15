d1a58ce0901569ae41b3f9f9c6de7b397e7057e2
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 08:47:55 -0700

    Add files via upload

---
+++ Applications/AzureADModule/builtinAppwOwnerAssigned.ps1
@@ -0,0 +1,6 @@
+﻿Connect-AzureAD
+$sps = Get-AzureADServicePrincipal -filter "servicePrincipalType eq 'Application'" -all $true | `
+    where {$_.PublisherName -like "*Microsoft*" -or !($_.PublisherName -eq "Microsoft Accounts") -and $_.AppOwnerTenantId -eq 'f8cdef31-a31e-4b4a-93e4-5f571e91255a'}
+
+$sps | where {Get-AzureADServicePrincipalOwner -ObjectId $_.objectid} | select objectid, displayname, PublisherName, AccountEnabled | export-csv .\builtin_apps_have_owners.csv -NoTypeInformation
+
