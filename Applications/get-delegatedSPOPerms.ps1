f4944d649952753ba0fb9d23e3bccefcbaf048c0
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 10:09:14 -0700

    Update get-delegatedSPOPerms.ps1

--- Applications/get-delegatedSPOPerms.ps1
+++ Applications/get-delegatedSPOPerms.ps1
@@ -1,3 +1,8 @@
+param($defaultpath = "$env:USERPROFILE\Downloads")
+if(!(Get-MgContext)){
+    connect-mggraph -scopes "Application.Read.All","Directory.Read.All"
+}
+cd $defaultpath
 $permissions = "Sites.FullControl.All","Sites.Manage.All","Sites.Read.All","Sites.ReadWrite.All","Files.Read.All","Files.ReadWrite.All","File.Read.All"
 
 $sps = Get-MGBetaServicePrincipal -filter "servicePrincipalType eq 'Application'" -all | `
