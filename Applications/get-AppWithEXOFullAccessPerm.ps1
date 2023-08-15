4f76f34deae7f2bf8d24e4f2abeb1b77f471eaff
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 10:06:38 -0700

    Update get-AppWithEXOFullAccessPerm.ps1

--- Applications/get-AppWithEXOFullAccessPerm.ps1
+++ Applications/get-AppWithEXOFullAccessPerm.ps1
@@ -1,4 +1,8 @@
-connect-mggraph -scope 'Application.Read.All', 'Directory.Read.All'
+param($defaultpath = "$env:USERPROFILE\Downloads")
+if(!(Get-MgContext)){
+    connect-mggraph -scopes "Application.Read.All","Directory.Read.All"
+}
+cd $defaultpath
 
 $ResourceId = (Get-MgServicePrincipal -filter "AppId eq '00000002-0000-0ff1-ce00-000000000000'" | select `
     @{N="SPid";E={$_.id}},@{N="SP";E={$_.displayname}} -ExpandProperty approles | where value -eq 'full_access_as_app' | select *).SPid
