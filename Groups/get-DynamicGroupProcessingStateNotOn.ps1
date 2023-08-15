332a82a071d46b3d3aae86b35f8acec1f6e74272
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 13:22:19 -0700

    Create get-DynamicGroupProcessingStateNotOn.ps1

---
+++ Groups/get-DynamicGroupProcessingStateNotOn.ps1
@@ -0,0 +1,10 @@
+param($defaultpath = "$env:USERPROFILE\Downloads")
+if(!(Get-MgContext)){
+    connect-mggraph -scopes "Group.Read.All","Directory.Read.All","DirectoryRecommendations.Read.All"
+}
+cd $defaultpath
+
+Get-MgBetaGroup -filter "SecurityEnabled eq true" -all `
+    -property Id, Displayname, OnPremisesSyncEnabled, SecurityEnabled,GroupTypes, IsAssignableToRole,MembershipRuleProcessingState, ExpirationDateTime, CreatedDateTime, CreatedByAppId `
+    -ExpandProperty owners | where {!($_.onPremisesSyncEnabled -eq $true) -and ($_.GroupTypes -contains "DynamicMembership") -and !($_.MembershipRuleProcessingState -eq 'On')} | select `
+        Id, Displayname, OnPremisesSyncEnabled, SecurityEnabled,GroupTypes, IsAssignableToRole,MembershipRuleProcessingState, ExpirationDateTime, CreatedDateTime, CreatedByAppId
