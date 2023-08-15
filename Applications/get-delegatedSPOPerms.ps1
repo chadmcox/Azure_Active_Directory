bdd8569c23da4dc956b077b6cdc6a9462633453c
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 09:28:37 -0700

    Create get-delegatedSPOPerms.ps1

---
+++ Applications/get-delegatedSPOPerms.ps1
@@ -0,0 +1,10 @@
+$permissions = "Sites.FullControl.All","Sites.Manage.All","Sites.Read.All","Sites.ReadWrite.All","Files.Read.All","Files.ReadWrite.All","File.Read.All"
+
+$sps = Get-MGBetaServicePrincipal -filter "servicePrincipalType eq 'Application'" -all | `
+    where {!($_.PublisherName -like "*Microsoft*") -or $_.PublisherName -eq "Microsoft Accounts" -and !($_.AppOwnerOrganizationId -eq 'f8cdef31-a31e-4b4a-93e4-5f571e91255a')-and $_.appDisplayname -ne "Microsoft Assessments"}
+
+$sps | where{$sp=$null; $sp=$_
+   Get-MgBetaServicePrincipalOauth2PermissionGrant -ServicePrincipalId $sp.id -PipelineVariable AADOPG | foreach{
+        $_.scope -split " " | where {$_ -in $permissions}
+   }
+} | select appid,id, displayname, PublisherName, AccountEnabled | export-csv .\spo_delegated_permissions.csv -NoTypeInformation
