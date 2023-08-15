1df47ff8f11c41a467fb54f201eebe9fa7378510
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2023-08-15 10:06:59 -0700

    Update get-AppWithEXOFullAccessPerm.ps1

--- Applications/get-AppWithEXOFullAccessPerm.ps1
+++ Applications/get-AppWithEXOFullAccessPerm.ps1
@@ -4,10 +4,10 @@ if(!(Get-MgContext)){
 }
 cd $defaultpath
 
-$ResourceId = (Get-MgServicePrincipal -filter "AppId eq '00000002-0000-0ff1-ce00-000000000000'" | select `
+$ResourceId = (Get-MgBetaServicePrincipal -filter "AppId eq '00000002-0000-0ff1-ce00-000000000000'" | select `
     @{N="SPid";E={$_.id}},@{N="SP";E={$_.displayname}} -ExpandProperty approles | where value -eq 'full_access_as_app' | select *).SPid
-$RoleID = (Get-MgServicePrincipal -filter "AppId eq '00000002-0000-0ff1-ce00-000000000000'" | select `
+$RoleID = (Get-MgBetaServicePrincipal -filter "AppId eq '00000002-0000-0ff1-ce00-000000000000'" | select `
     @{N="SPid";E={$_.id}},@{N="SP";E={$_.displayname}} -ExpandProperty approles | where value -eq 'full_access_as_app' | select *).id
 
-Get-MgServicePrincipalAppRoleAssignedTo -ServicePrincipalId $ResourceId | where approleid -eq $RoleID | select `
+Get-MgBetaServicePrincipalAppRoleAssignedTo -ServicePrincipalId $ResourceId | where approleid -eq $RoleID | select `
     PrincipalDisplayName, PrincipalId, PrincipalType, ResourceDisplayName, ResourceId
