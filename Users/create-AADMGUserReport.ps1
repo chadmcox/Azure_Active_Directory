3878979d273d91ec37d8205d3926e370a99f47cd
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2024-01-02 11:14:53 -0700

    Update create-AADMGUserReport.ps1

--- Users/create-AADMGUserReport.ps1
+++ Users/create-AADMGUserReport.ps1
@@ -1,12 +1,11 @@
 param($defaultpath="$env:USERPROFILE\downloads",$pwdnochangedindays = 480)
 cd $defaultpath
 
-Connect-MgGraph -Scopes "Directory.ReadWrite.All", "Directory.AccessAsUser.All","User.Read.All","AuditLog.Read.All" 
-Select-MgProfile -Name beta
+Connect-MgGraph -Scopes "Directory.ReadWrite.All", "Directory.AccessAsUser.All","User.Read.All","AuditLog.Read.All"
 
-Get-MgUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName, `
+Get-MgBetaUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName, `
   userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,passwordPolicies,mail,lastPasswordChangeDateTime | `
     select id,displayName,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,mail,lastPasswordChangeDateTime,passwordPolicies, `
-        @{N='LastSignInDateTime';E={$_.signInActivity.LastSignInDateTime}}, @{N='LastNonInteractiveSignInDateTime';E={$_.signInActivity.LastNonInteractiveSignInDateTime}} | `
-          export-csv .\aad_user_report.csv -notypeinformation
+      @{N='lastSuccessfulSignInDateTime';E={$_.signInActivity.lastSuccessfulSignInDateTime}}, @{N='LastSignInDateTime';E={$_.signInActivity.LastSignInDateTime}}, `
+      @{N='LastNonInteractiveSignInDateTime';E={$_.signInActivity.LastNonInteractiveSignInDateTime}} | export-csv .\aad_user_report.csv -notypeinformation
 write-host "Report can be found here cd $defaultpath"
