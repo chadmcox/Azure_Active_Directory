04aaa0739890170d794ac6f48adcdaac6a3f6d86
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2024-07-10 13:32:26 -0700

    Update count-usersperconditionalaccesspolicy.ps1

--- Conditional Access Policy/count-usersperconditionalaccesspolicy.ps1
+++ Conditional Access Policy/count-usersperconditionalaccesspolicy.ps1
@@ -46,6 +46,17 @@ function get-MSGraphRequest{
         }until ($uri -eq $null)
 }
 
+function resolve-app{
+    [cmdletbinding()] 
+        param($appid)
+        if($appid -match("^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$")){
+            (Get-MgBetaServicePrincipal -Filter "appId eq '$appid'").displayName
+        }else{
+            $appid
+        }
+}
+
+
 #login
 login-MSGraph
 #export all enabled conditional access policies
@@ -58,7 +69,7 @@ $totalGuest = Get-MgBetaUserCount -ConsistencyLevel "Eventual" -filter "userType
 $all_capolicies | foreach {$cap="";$cap=$_
     $IncludedUsers = 0
     if($cap.conditions.users.includeUsers -contains "All"){
-        $IncludedUsers = $totalUsers
+        $IncludedUsers = "$($totalUsers + $totalGuest) (All)"
     }else{
         $IncludedUsers = ($cap.conditions.users.includeUsers).count
     }
@@ -98,6 +109,26 @@ $all_capolicies | foreach {$cap="";$cap=$_
     if(($cap.conditions.users.excludeGuestsOrExternalUsers | measure).count -gt 0){
         $ExcludedUsers += $totalGuest
     }
-    $cap | select @{n='ConditionalAccessPolicyName';e={$_.displayName}},state, @{n='TargetedUserCount';e={$IncludedUsers}},@{n='ExcludedUserCount';e={$ExcludedUsers}}
+    $cap | select @{n='ConditionalAccessPolicyName';e={$_.displayName}}, `
+        state, @{n='TotalEnabledUsers';e={"Users: $totalUsers Guest: $totalGuest"}}, `
+        @{n='TargetedUserCount';e={$IncludedUsers}}, `
+        @{n='ExcludedUserCount';e={$ExcludedUsers}}, `
+        @{n='GrantControl';e={($cap.grantControls.builtInControls -join(" $($cap.grantControls.operator) "))}}, `
+        @{n='included: Applications';e={($_.conditions.applications.includeApplications | foreach{resolve-app -appid $_}) -join(";")}}, `
+        @{n='excluded: Applications';e={($_.conditions.applications.excludeApplications | foreach{resolve-app -appid $_}) -join(";")}}, `
+        @{n='includePlatforms';e={if($_.conditions.platforms.includePlatforms -eq 'All'){'All'}else{$(($_.conditions.platforms.includePlatforms) -join(";"))}}}, `
+        @{n='excludePlatforms';e={if($_.conditions.platforms.excludePlatforms -eq 'All'){'All'}else{$(($_.conditions.platforms.excludePlatforms) -join(";"))}}}, `
+        @{n='includeLocations';e={if($_.conditions.locations.includeLocations -eq 'All'){'All'}else{($_.conditions.locations.includeLocations | measure).count}}}, `
+        @{n='excludeLocations';e={if($_.conditions.locations.excludeLocations -eq 'AllTrusted'){'AllTrusted'}else{($_.conditions.locations.excludeLocations | measure).count}}}, `
+        @{n='clientAppTypes';e={if($_.conditions.clientAppTypes -eq 'all'){'All'}else{$(($_.conditions.clientAppTypes) -join(";"))}}}, `
+        @{n='deviceFilter';e={if($_.conditions.devices.deviceFilter){$true}else{}}}, `
+        @{n='disableResilienceDefaults';e={$_.grantControls.sessionControls.disableResilienceDefaults}}, `
+        @{n='applicationEnforcedRestrictions';e={$_.sessionControls.applicationEnforcedRestrictions.isenabled}}, `
+        @{n='cloudAppSecurity';e={$_.sessionControls.cloudAppSecurity.cloudAppSecurityType}}, `
+        @{n='continuousAccessEvaluation';e={$_.sessionControls.continuousAccessEvaluation}}, `
+        @{n='secureSignInSession';e={$_.sessionControls.secureSignInSession}}, `
+        @{n='signInFrequency';e={if($_.sessionControls.signInFrequency.isenabled){"$($_.sessionControls.signInFrequency.isenabled) / $($_.sessionControls.signInFrequency.frequencyInterval)"}}}, `
+        @{n='persistentBrowser';e={$_.sessionControls.persistentBrowser.mode}}, `
+        TokenProtection
     
 } | export-csv .\usercountscopedforeachconditionalaccesspolicy.csv -NoTypeInformation
