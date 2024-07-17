160566b99682ed2454a3c6f72608ce27bdddd5b2
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2024-07-17 10:27:23 -0700

    Update find-AADMGUsersStaleLastSignIn.ps1

--- Users/find-AADMGUsersStaleLastSignIn.ps1
+++ Users/find-AADMGUsersStaleLastSignIn.ps1
@@ -5,6 +5,7 @@ Select-MgProfile -Name beta
 
 Get-MgUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,passwordPolicies,mail,lastPasswordChangeDateTime | `
     select id,displayName,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,mail,lastPasswordChangeDateTime,passwordPolicies, `
+        @{Name="lastSuccessfulSignInDateTime";Expression={(get-date $_.signInActivity.lastSuccessfulSignInDateTime).tostring('yyyy-MM-dd')}}, `
         @{N='LastSignInDateTime';E={$_.signInActivity.LastSignInDateTime}}, @{N='LastNonInteractiveSignInDateTime';E={$_.signInActivity.LastNonInteractiveSignInDateTime}} | `
             where {($_.LastSignInDateTime -eq $null)  -or ((New-TimeSpan -Start $_.LastSignInDateTime -end $(get-date)).TotalDays -gt $notsignedonindays)} | `
             where {($_.LastNonInteractiveSignInDateTime -eq $null) -or ((New-TimeSpan -Start $_.LastNonInteractiveSignInDateTime -end $(get-date)).TotalDays -gt $notsignedonindays)} | `
