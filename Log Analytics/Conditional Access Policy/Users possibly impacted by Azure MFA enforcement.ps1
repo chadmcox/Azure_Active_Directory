68549e2bbe33f371d9c5423cc8a2b37b80c03ed1
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2024-07-23 14:16:56 -0700

    Create Users possibly impacted by Azure MFA enforcement.ps1

---
+++ Log Analytics/Conditional Access Policy/Users possibly impacted by Azure MFA enforcement.ps1
@@ -0,0 +1,2 @@
+//link to blog post https://techcommunity.microsoft.com/t5/core-infrastructure-and-security/update-on-mfa-requirements-for-azure-sign-in/ba-p/4177584
+//this will get all users successfully signing into the azure endpoints that are going to get mfa enforcement
