f0d988c9a520bac83fac156aba261021506f46a2
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2024-09-19 09:23:39 -0700

    Update find-AADMGUsersStaleLastSignIn.ps1

--- Users/find-AADMGUsersStaleLastSignIn.ps1
+++ Users/find-AADMGUsersStaleLastSignIn.ps1
@@ -1,9 +1,9 @@
 param($defaultpath="$env:USERPROFILE\downloads",$notsignedonindays = 120)
 cd $defaultpath
 Connect-MgGraph -Scopes "Directory.ReadWrite.All", "Directory.AccessAsUser.All","User.Read.All","AuditLog.Read.All" 
-Select-MgProfile -Name beta
+#Select-MgProfile -Name beta
 
-Get-MgUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,passwordPolicies,mail,lastPasswordChangeDateTime | `
+Get-MgBetaUser -Filter "userType eq 'Member' and AccountEnabled eq true" -all  -Property id,displayName,signInActivity,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,passwordPolicies,mail,lastPasswordChangeDateTime | `
     select id,displayName,userPrincipalName,userType,onPremisesSyncEnabled,createdDateTime,accountEnabled,mail,lastPasswordChangeDateTime,passwordPolicies, `
         @{Name="lastSuccessfulSignInDateTime";Expression={(get-date $_.signInActivity.lastSuccessfulSignInDateTime).tostring('yyyy-MM-dd')}}, `
         @{N='LastSignInDateTime';E={$_.signInActivity.LastSignInDateTime}}, @{N='LastNonInteractiveSignInDateTime';E={$_.signInActivity.LastNonInteractiveSignInDateTime}} | `
