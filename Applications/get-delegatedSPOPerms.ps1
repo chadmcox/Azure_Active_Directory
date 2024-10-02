050aabaa0657fb2abb14dafbc25654b1b65b0971
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2024-10-02 13:47:23 -0700

    Update get-delegatedSPOPerms.ps1

--- Applications/get-delegatedSPOPerms.ps1
+++ Applications/get-delegatedSPOPerms.ps1
@@ -3,7 +3,7 @@ if(!(Get-MgContext)){
     connect-mggraph -scopes "Application.Read.All","Directory.Read.All"
 }
 cd $defaultpath
-$permissions = "Sites.FullControl.All","Sites.Manage.All","Sites.Read.All","Sites.ReadWrite.All","Files.Read.All","Files.ReadWrite.All","File.Read.All"
+$permissions = "Sites.FullControl.All","Sites.Manage.All","Sites.Read.All","Sites.ReadWrite.All","Files.Read.All","Files.ReadWrite.All","File.Read.All", "Files.readwrite"
 
 $sps = Get-MGBetaServicePrincipal -filter "servicePrincipalType eq 'Application'" -all | `
     where {!($_.PublisherName -like "*Microsoft*") -or $_.PublisherName -eq "Microsoft Accounts" -and !($_.AppOwnerOrganizationId -eq 'f8cdef31-a31e-4b4a-93e4-5f571e91255a')-and $_.appDisplayname -ne "Microsoft Assessments"}
