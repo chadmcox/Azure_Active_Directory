2c34f9f5aa28800e9c9bc4b1219219a4c81568bd
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2024-12-19 10:13:09 -0700

    Update activate-UserHardwareOathToken.ps1

--- Hardware Oath Token/activate-UserHardwareOathToken.ps1
+++ Hardware Oath Token/activate-UserHardwareOathToken.ps1
@@ -1,10 +1,34 @@
+<#PSScriptInfo
+.VERSION 2024.19
+.GUID 8580e442-6a53-44cc-b821-2fe2d7fda178
+.AUTHOR Chad.Cox@microsoft.com
+    https://blogs.technet.microsoft.com/chadcox/
+    https://github.com/chadmcox
+.COMPANYNAME 
+.COPYRIGHT This Sample Code is provided for the purpose of illustration only and is not
+intended to be used in a production environment.  THIS SAMPLE CODE AND ANY
+RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  We grant You a
+nonexclusive, royalty-free right to use and modify the Sample Code and to
+reproduce and distribute the object code form of the Sample Code, provided
+that You agree: (i) to not use Our name, logo, or trademarks to market Your
+software product in which the Sample Code is embedded; (ii) to include a valid
+copyright notice on Your software product in which the Sample Code is embedded;
+and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and
+against any claims or lawsuits, including attorneys` fees, that arise or result
+from the use or distribution of the Sample Code..
+.description
+url:  How to manage hardware OATH tokens in Microsoft Entra ID (Preview) - https://learn.microsoft.com/en-us/entra/identity/authentication/how-to-mfa-manage-oath-tokens
+https://learn.microsoft.com/en-us/graph/api/resources/hardwareoathtokenauthenticationmethoddevice?view=graph-rest-beta
+#>
 
 param($defaultdirectory="$env:USERPROFILE\Downloads")
 cd $defaultdirectory
 
 cls
 
-Start-Transcript -path $env:USERPROFILE\Downloads\transcript-newhardwaretoken.txt
+Start-Transcript -path .\transcript-activatehardwaretoken.txt
 
 Disconnect-MgGraph
 #display a list of clouds for the user to select and sign-in to.
@@ -35,6 +59,19 @@ function getuserhardwareoathtokens{
         $results | out-host
     }
 }
+
+Function activateUserHardwareOathToken{
+    param($userid, $oathtokenid, $vcode)
+    $verificationcode = @{}
+    $verificationcode.add("verificationCode","$vcode")
+    $uri = "$graphendpoint/beta/users/$userid/authentication/hardwareOathMethods/$oathtokenid/activate"
+    Invoke-MgGraphRequest -Method POST -Uri $uri -ContentType "application/json" -Body ($verificationcode | convertto-json -Depth 99)
+    start-sleep -Seconds 3
+    getuserhardwareoathtokens -userid $userid
+    write-host "Validate hardware token $oathtokenid has been activated"
+}
+
+
 cls
 
 $input_upn=$null;$input_upn = Read-Host "Type the UPN of the user"
@@ -46,4 +83,9 @@ cls
 
 getuserhardwareoathtokens -userid $user_id
 
-$input_token_id_to_activate = Read-Host "Scroll up find the token you want to activate, then copy the id and paste here, then press enter"
+$input_token_id_to_activate = Read-Host "Scroll up, then copy the id of the token you want to activate and paste here, then press enter"
+$input_verification_code = Read-Host "Enter the code from the hardware token, then press enter"
+cls
+activateUserHardwareOathToken -userid $user_id -oathtokenid $input_token_id_to_activate -vcode $input_verification_code
+
+Stop-Transcript
