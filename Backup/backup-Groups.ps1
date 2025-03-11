4164405fdab7ebe6c5c00a861d567ab9e045d247
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2025-03-11 09:39:44 -0700

    Update and rename backup-Groups.ps1 to export-Groups.ps1

--- Backup/backup-Groups.ps1
+++
@@ -1,4 +0,0 @@
-#export Existing Azure AD Security Groups
-get-azureadgroup -Filter "SecurityEnabled eq true" -All $true | `
-    select ObjectId, DisplayName, DirSyncEnabled | `
-        export-csv ".\Azure_AD_Security_Groups_$((Get-AzureADTenantDetail).DisplayName)_$(get-date -f yyyy-MM-dd).csv" -NoTypeInformation
