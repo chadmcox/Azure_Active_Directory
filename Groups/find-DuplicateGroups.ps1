4e19063df9fa6b17dab329b96d0d8cdddb7e4177
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-06-22 15:06:07 -0700

    Update find-DuplicateGroups.ps1

--- Groups/find-DuplicateGroups.ps1
+++ Groups/find-DuplicateGroups.ps1
@@ -6,6 +6,11 @@ cd $defaultpath
 
 $file1 = ".\original_groups.csv"
 $file2 = ".\duplicate_groups.csv"
+$file3 = ".\final_duplicate_groups.csv"
 
-Get-MgBetaGroup -All -Filter "onPremisesSyncEnabled eq true" | select createdDateTime, displayName, expirationDateTime,onPremisesDomainName, onPremisesLastSyncDateTime, onPremisesNetBiosName, onPremisesProvisioningErrors, onPremisesSecurityIdentifier,         onPremisesSyncEnabled, securityEnabled | export-csv $file1 -NoTypeInformation
+Get-MgBetaGroup -All -Filter "onPremisesSyncEnabled eq true" | select createdDateTime, displayName, expirationDateTime,onPremisesDomainName, onPremisesLastSyncDateTime, onPremisesNetBiosName, onPremisesSecurityIdentifier,onPremisesSyncEnabled, securityEnabled | export-csv $file1 -NoTypeInformation
 import-csv $file1 | group displayname | select name, count | sort count -Descending | where count -gt 1 | export-csv $file2 -NoTypeInformation
+$duplicate = import-csv $file2 | group name -AsHashTable -AsString
+
+write-host "total duplicates $($duplicate.count)"
+import-csv $file1 | where {$duplicate.Contains($_.displayname)} | export-csv $file3 -NoTypeInformation
