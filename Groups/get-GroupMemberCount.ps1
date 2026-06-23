de39986e3b6af17f190f1938af05443d1c5c1ce1
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-06-23 12:16:56 -0700

    Update get-GroupMemberCount.ps1

--- Groups/get-GroupMemberCount.ps1
+++ Groups/get-GroupMemberCount.ps1
@@ -6,12 +6,12 @@ cd $defaultpath
 
 $file1 = ".\member_count_groups.csv"
 
-$groups = Get-MgBetaGroup -All -Property Id,DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled | select  Id,DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled
+$groups = Get-MgBetaGroup -All -Property Id,DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled,onPremisesDomainName, onPremisesLastSyncDateTime | select  Id,DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled,onPremisesDomainName, onPremisesLastSyncDateTime
 
  $groups | ForEach-Object {$count=0
     $count = (Invoke-MgGraphRequest -Method GET `
         -Uri "https://graph.microsoft.com/v1.0/groups/$($_.Id)/members/`$count" -Headers @{ConsistencyLevel="eventual"})
 
-        $_ | Select-Object Id, DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled, @{N="membercount";E={$count}}
+        $_ | Select-Object Id, DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled, @{N="membercount";E={$count}}, onPremisesDomainName, onPremisesLastSyncDateTime
 
 } | export-csv $file1 -notypeinformation
