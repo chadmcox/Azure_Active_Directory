c1b396c580c0763a002292325f915fb01f140a7e
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-06-23 10:49:23 -0700

    Create get-GroupMemberCount.ps1

---
+++ Groups/get-GroupMemberCount.ps1
@@ -0,0 +1,15 @@
+param($defaultpath = "$env:USERPROFILE\Downloads")
+if(!(Get-MgContext)){
+    connect-mggraph -scopes "Group.Read.All","Directory.Read.All"
+}
+cd $defaultpath
+
+$file1 = ".\member_count_groups.csv"
+
+Get-MgBetaGroup -All -Property Id,DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled | ForEach-Object {$count=0
+    $count = (Invoke-MgGraphRequest -Method GET `
+        -Uri "https://graph.microsoft.com/v1.0/groups/$($_.Id)/members/`$count" -Headers @{ConsistencyLevel="eventual"})
+
+        $_ | Select-Object Id, DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled, @{N="membercount";E={$count}}
+
+} | export-csv $file1 -notypeinformation
