2f7aa34a8183b32bef0185e8bdbd112dab5f0339
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-06-26 09:52:31 -0700

    Update get-GroupMemberCount.ps1

--- Groups/get-GroupMemberCount.ps1
+++ Groups/get-GroupMemberCount.ps1
@@ -7,11 +7,13 @@ cd $defaultpath
 $file1 = ".\member_count_groups.csv"
 
 $groups = Get-MgBetaGroup -All -Property Id,DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled,onPremisesDomainName, onPremisesLastSyncDateTime,createdDateTime, isAssignableToRole,onPremisesSecurityIdentifier | select  Id,DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled,onPremisesDomainName, onPremisesLastSyncDateTime,createdDateTime,isAssignableToRole,onPremisesSecurityIdentifier
-
- $groups | ForEach-Object {$count=0
+$totalcount = $groups.count
+$i = 0
+ $groups | ForEach-Object {$count=0 ; $i++
+    Write-host "Checking $($_.displayname) $i of $totalcount"
     $count = (Invoke-MgGraphRequest -Method GET `
         -Uri "https://graph.microsoft.com/v1.0/groups/$($_.Id)/members/`$count" -Headers @{ConsistencyLevel="eventual"})
-
+    Write-host "Checking $($_.displayname) has $count members"
         $_ | Select-Object Id, DisplayName,onPremisesSyncEnabled,mailEnabled, SecurityEnabled, @{N="membercount";E={$count}}, onPremisesDomainName, onPremisesLastSyncDateTime,createdDateTime,isAssignableToRole,onPremisesSecurityIdentifier
 
 } | export-csv $file1 -notypeinformation
