9d9fee39f8e958754d0045ac4b2b6abde16257fe
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-08-03 10:41:19 -0700

    Update get-GroupMemberCount.ps1

--- Groups/get-GroupMemberCount.ps1
+++ Groups/get-GroupMemberCount.ps1
@@ -4,13 +4,14 @@ if(!(Get-MgContext)){
 }
 cd $defaultpath
 
-$file1 = ".\member_count_groups.csv"
+$file1 = ".\Entra_Security_Groups_Member_Count.csv"
+$file2 = ".\Entra_Security_Groups.csv"
 
-function GetAADGroupCounts {
+function GetEntraGroups {
     [cmdletbinding()]
     param()
 
-    Write-Host "Retrieving groups...writing to this file $file1 in this directory $defaultpath"
+    Write-Host "Retrieving groups...writing to this file $file2 in this directory $defaultpath"
 
     $uri = "https://graph.microsoft.com/beta/groups?`$filter=securityEnabled eq true and mailEnabled eq false&`$select=id,displayName,groupTypes,OnPremisesSyncEnabled,mailEnabled,SecurityEnabled,onPremisesDomainName, onPremisesLastSyncDateTime,createdDateTime,isAssignableToRole,onPremisesSecurityIdentifier&`$top=999"
 
@@ -36,42 +37,47 @@ function GetAADGroupCounts {
             }
         }
 
-        $results.value |
-            Select-Object displayName,
-                          id,
-                          OnPremisesSyncEnabled,mailEnabled, SecurityEnabled,
-                          @{N="DynamicGroup";E={$_.groupTypes -contains "DynamicMembership"}},
-                          @{N="MemberCount";E={
-
-                                $countUri = "https://graph.microsoft.com/beta/groups/$($_.id)/members/`$count"
-                                $count    = -1
-
-                                for($x = 0; $x -le 3; $x++) {
-                                    try {
-                                        $count = Invoke-MgGraphRequest -Uri $countUri -Method GET -Headers @{ConsistencyLevel="eventual"}
-                                        break
-                                    }
-                                    catch {
-                                        if(($_.Exception.Response.StatusCode -eq "TooManyRequests") -or
-                                           ($_.Exception.Response.StatusCode.value__ -eq 429)) {
-
-                                            Start-Sleep -Seconds $_.Exception.Response.Headers.RetryAfter.Delta.Seconds
-                                        }
-                                        else {
-                                            Write-Host $_.Exception.Message
-                                            break
-                                        }
-                                    }
-                                }
-
-                                [int]$count
-                          }},onPremisesDomainName, onPremisesLastSyncDateTime,createdDateTime,isAssignableToRole,onPremisesSecurityIdentifier
+        $results.value
 
         $uri = $results.'@odata.nextLink'
 
     } until ($null -eq $uri)
 }
 
-GetAADGroupCounts | export-csv $file1 -notypeinformation
+function getentragroupcount {
+    [cmdletbinding()]
+    param($groupid)
+    $countUri = "https://graph.microsoft.com/beta/groups/$($_.id)/members/`$count"
+    $count    = -1
+
+    for($x = 0; $x -le 3; $x++) {
+        try {
+            $count = Invoke-MgGraphRequest -Uri $countUri -Method GET -Headers @{ConsistencyLevel="eventual"}
+            break
+        }
+        catch {
+            if(($_.Exception.Response.StatusCode -eq "TooManyRequests") -or
+                ($_.Exception.Response.StatusCode.value__ -eq 429)) {
+
+                Start-Sleep -Seconds $_.Exception.Response.Headers.RetryAfter.Delta.Seconds
+            }
+            else {
+                Write-Host $_.Exception.Message
+                break
+            }
+        }
+    }
+
+    [int]$count          
+}
+
+
+$groups = GetEntraGroups
+$groups | Select-Object displayName,id,OnPremisesSyncEnabled,mailEnabled, SecurityEnabled,@{N="DynamicGroup";E={$_.groupTypes -contains "DynamicMembership"}}, `
+                onPremisesDomainName, onPremisesLastSyncDateTime,createdDateTime,isAssignableToRole,onPremisesSecurityIdentifier| export-csv $file2 -NoTypeInformation
+Write-Host "Retrieved $($Groups.Count) groups"
+Write-Host "Now Retrieving group member count writing to $file1"
+$groups | Select-Object displayName,id,OnPremisesSyncEnabled,mailEnabled, SecurityEnabled,@{N="DynamicGroup";E={$_.groupTypes -contains "DynamicMembership"}}, `
+                @{N="MemberCount";E={getentragroupcount -groupid $($_.id)}},onPremisesDomainName, onPremisesLastSyncDateTime,createdDateTime,isAssignableToRole,onPremisesSecurityIdentifier | export-csv $file1 -notypeinformation
 
 Write-Host "Complete"
